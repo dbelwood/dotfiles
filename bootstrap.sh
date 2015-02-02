3c3613c324812eefea5971dceb34d28751812c2c
Dan Belwood <dan.belwood@gmail.com>
2015-02-01 19:09:53 -0600

    A bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+# Install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew formulae
+apps=(brew-cask chruby go git hub ruby-install task)
+for app ($apps); do
+  brew install $app
+done
+
+# Install a system ruby
+ruby-install ruby 2.1.5
+chruby ruby-2.1.5
+
+# Install up
+gem install up
+
+# Install casks
+cask_apps=(dropbox flux google-chrome iterm2 macvim)
+for app ($cask_apps); do
+  brew cask install $app
+done
+
+# Install oh-my-zsh
+rm -rf ~/.oh-my-zsh # Clear out old install
+curl -L http://install.ohmyz.sh | sh
+
+# Bootstrap home directory
+# zsh
+cp -rf .zsh* ~
+
+# vim
+cp -rf .vim* ~
+
+echo "All done!"
