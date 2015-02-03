269e11c3644d476b0fa493e3a35f5aebbf4205dd
Dan Belwood <dan.belwood@gmail.com>
2015-02-02 20:59:15 -0600

    Change is_brewed to is_not_brewed, i.e. happy path

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,18 +5,18 @@ if [ $(which brew | grep 'not found' | wc -l) -eq 1 ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-is_brewed() {
+is_not_brewed() {
   if [[ -z "$2" ]]; then
-    test $(brew cask info $1 | grep 'Not installed' | wc -l) -eq 1
+    test $(brew cask info $1 | grep 'Not installed' | wc -l) -eq 0
   else
-    test $(brew ls --versions $1 | wc -l) -eq 1
+    test $(brew ls --versions $1 | wc -l) -eq 0
   fi
 }
 
 # Install brew formulae
 apps=(brew-cask chruby go git hub ruby-install task)
 for app ($apps); do
-  if [ ! is_brewed $app ]; then
+  if is_not_brewed $app; then
     brew install $app
   fi
 done
@@ -24,7 +24,7 @@ done
 # Install casks
 cask_apps=(dropbox flux google-chrome iterm2 macvim)
 for app ($cask_apps); do
-  if [! is_brewed $app 1]; then
+  if is_not_brewed $app 1; then
     brew cask install $app
   fi
 done
