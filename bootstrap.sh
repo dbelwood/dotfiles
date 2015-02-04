7ea052726d3bc5d6d653224272b839951e6e1cee
Dan Belwood <dan.belwood@gmail.com>
2015-02-03 19:34:52 -0600

    Adds comments and ruby_ver var

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,55 +14,73 @@ is_not_brewed() {
 }
 
 # Install brew formulae
+echo "Installing brew formulas"
 apps=(brew-cask chruby go git hub ruby-install task)
 for app ($apps); do
   if is_not_brewed $app; then
     brew install $app
   fi
 done
+echo "done"
 
 # Install casks
+echo "Installing brew casks"
 cask_apps=(dropbox flux google-chrome iterm2 macvim)
 for app ($cask_apps); do
   if is_not_brewed $app 1; then
     brew cask install $app
   fi
 done
+echo "done"
 
 # Back yo sh*t up
+echo "Backing up rc files"
 files=(.vimrc .zshrc .gitconfig .vimrc.plugins)
 for file ($files); do
   if [[ ! -e $file ]]; then
     continue
   fi 
+  echo "Backing up $file"
   if [[ -h $file ]]; then
     rm -f $file
   else
     mv $file $file.bak
   fi
 done
+echo "done."
 
 # Install oh-my-zsh
-if [[ -z ~/.oh-my-zsh ]]; then
+if [[ ! -e ~/.oh-my-zsh ]]; then
+  echo "Install Oh My ZSH"
   curl -L http://install.ohmyz.sh | sh
+  echo "done"
 fi
 
 # Set gitconfig settings
+echo "Link git configuration"
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+echo "done"
 
 # Bootstrap zsh
+echo "Link zsh configuration"
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 
 source ~/.zshrc
+echo "done"
 
 # Ruby installation
-#ruby-install ruby 2.1.5 # Install a system ruby
-chruby ruby-2.1.5 # Set system ruby
+local ruby_ver="2.1.5"
+echo "Install chruby and ruby version $ruby_ver"
+ruby-install ruby $ruby_ver # Install a system ruby
+chruby ruby-$ruby_ver # Set system ruby
 gem install up # Install up
+echo "done"
 
 # vim
+echo "Link vim configuration"
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/vimrc.plugins ~/.vimrc.plugins
+echo "done"
 
 env zsh
 . ~/.zshrc
