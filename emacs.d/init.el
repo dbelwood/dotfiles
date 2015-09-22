84613f7e095bf75b79247dd0009fc77a6904f1bd
Dan Belwood <dan.belwood@gmail.com>
2015-09-22 17:25:28 -0500

    Add org mode and proper Emacs lisp compliant docs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,11 @@
+;;; init --- Emacs Initialization
+
+;;; Commentary:
+; This is my gigantic init file.  I'll keep it as one file until this
+; becomes unweildy
+
+;;; Code:
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package Management
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -30,18 +38,21 @@
 ;; Theme settings
 (load-theme 'solarized t)
 (defun set-background-mode (frame mode)
+  "Change the background of the current editor from to light or dark.  FRAME is an editor frame.  MODE should either be 'light or 'dark."
   (set-frame-parameter frame 'background-mode mode)
   (when (not (display-graphic-p frame))
     (set-terminal-parameter (frame-terminal frame) 'background-mode mode))
   (enable-theme 'solarized))
 
 (defun switch-theme ()
+  "Switch the theme to light or dark solarized."
   (interactive)
   (let ((mode (if (eq (frame-parameter nil 'background-mode) 'dark)
 		   'light 'dark)))
     (set-background-mode nil mode)))
 
 (custom-set-variables '(solarized-termcolors 256))
+(defvar solarized-default-background-mode)
 (setq solarized-default-background-mode 'dark)
 
 (add-hook 'after-make-frame-functions
@@ -66,28 +77,28 @@
 (ido-mode t)
 
 ;; Evil mode settings
+(defvar evil-toggle-key)
 (setq evil-toggle-key "C-q")
 (evil-mode 1)
 (global-evil-surround-mode 1)
 
 ;; fix tabs
 (defun generate-tab-stops (&optional width max)
-  "Return a tab stop sequence"
+  "Return a tab stop sequence.  WIDTH is an optional override for the default tab stop of 2 spaces.  MAX is the max number of columns to calculate tab stops."
   (let* ((max-column (or max 100))
-	 (tab-width (or width tab-width))
+	 (tab-width (or width 2))
 	 (count (/ max-column tab-width)))
     (number-sequence tab-width (* tab-width count) tab-width)))
 
-(setq tab-width 2)
 (setq tab-stop-list (generate-tab-stops))
 (setq-default indent-tabs-mode nil)
 
 (defun copy-from-osx ()
-  "Copy from the system clipboard"
+  "Copy from the system clipboard."
   (shell-command-to-string "pbpaste"))
 
-(defun paste-to-osx (text &optional push)
-  "Copy to the system clipboard"
+(defun paste-to-osx (text)
+  "Copy to the system clipboard.  TEXT is the text to copy to the clipboard."
   (let ((process-connection-type nil))
     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
       (process-send-string proc text)
@@ -106,16 +117,18 @@
 
 (require 'git-blame)
 (require 'magit)
+
+(defvar magit-last-seen-setup-instructions)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 (global-set-key (kbd "C-x g") 'magit-status)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Misc 
+;; Misc
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; store all backup and autosave files in the tmp dir
-(setq auto-save-background-directory "~/.emacs-auto-save")
+(defvar auto-save-background-directory)
+(setq auto-save-background-directory "~/.emacs-auto-save") ; store all backup and autosave files in the tmp dir
 (setq backup-directory-alist
       `((".*" . ,auto-save-background-directory)))
 (setq auto-save-file-name-transforms
@@ -138,6 +151,9 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require 'org)
+(setq org-log-done t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Language settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -146,3 +162,6 @@
 (load "my-sql.el")
 (load "my-golang.el")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provide 'init)
+;;; init.el ends here
