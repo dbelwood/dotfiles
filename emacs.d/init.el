1a1f2602da2608e2a7d08acba6e09857ad8dad9a
Dan Belwood <dan.belwood@gmail.com>
2020-01-03 14:58:01 -0600

    Cleanup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,15 +6,11 @@
 
 ;;; Code:
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package Management
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '(("melpa-stable" . "https://stable.melpa.org/packages/")
-               ("melpa" . "https://melpa.org/packages/"))
-             t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -27,27 +23,21 @@
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;; Configuration management
 ;; Access important environment variables, i.e. $PATH
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
   :ensure t
   :config
+  (exec-path-from-shell-copy-env "JAVA_HOME")
   (exec-path-from-shell-initialize))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UI Settings
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq inhibit-splash-screen t
-      initial-scratch-message nil
-      initial-major-mode 'org-mode
-      global-linum-mode t ; show line numbers
-      column-number-mode t ; show current row/column in modeline
-      display-time-mode t
-      )
+      initial-scratch-message nil)
+(global-linum-mode t) ; show line numbers
+(column-number-mode t) ; show current row/column in modeline
+(display-time-mode t)
 
 ;; Map OSX keys correctly
 (setq mac-option-modifier 'meta
@@ -60,27 +50,29 @@
 ;; revert buffers automatically when underlying files are changed externally
 (global-auto-revert-mode t)
 
+(setq make-backup-files nil) ;; Don't save backups
+
 ;; Encoding
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
 ;; Theme settings
-(use-package spacemacs-theme
-  :ensure t
-  :pin melpa)
+(use-package spacemacs-common
+  :ensure spacemacs-theme
+  :pin melpa
+  :config
+  (load-theme 'spacemacs-dark t))
+
+;; Font
+(set-frame-font "Hack 16" nil t)
 
 (use-package ace-window
   :ensure t
   :bind
   ("M-o" . ace-window))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Search settings
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package helm
   :ensure t
   :config
@@ -95,23 +87,15 @@
   (setq projectile-enable-caching t)
   (setq projectile-completion-system 'grizzl)
   :config
-  (projectile-global-mode)
+  (projectile-mode))
+
+(use-package helm-projectile
+  :ensure t
+  :config
   (helm-projectile-on))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Keyboard settings
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; fix tabs
-;; (defun generate-tab-stops (&optional width max)
-;;   "Return a tab stop sequence.  WIDTH is an optional override for the default tab stop of 2 spaces.  MAX is the max number of columns to calculate tab stops."
-;;   (let* ((max-column (or max 100))
-;; 	 (tab-width (or width 2))
-;; 	 (count (/ max-column tab-width)))
-;;     (number-sequence tab-width (* tab-width count) tab-width)))
-
-;; (setq tab-stop-list (generate-tab-stops))
 (setq tab-width 2
       indent-tabs-mode nil)
 
@@ -128,57 +112,35 @@
 
 (setq interprogram-cut-function 'paste-to-osx)
 (setq interprogram-paste-function 'copy-from-osx)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; git settings
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package magit
   :ensure t
+  :pin melpa-stable
   :bind
   ("C-x g" . magit-status))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Misc
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (defvar auto-save-background-directory)
-;; (setq auto-save-background-directory "~/.emacs-auto-save") ; store all backup and autosave files in the tmp dir
-;; (setq backup-directory-alist
-;;       `((".*" . ,auto-save-background-directory)))
-;; (setq auto-save-file-name-transforms
-;;       `((".*" ,auto-save-background-directory  t)))
-
-;; (require 'ag)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Auto Complete config
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package company
+  :ensure t
   :config
   (global-company-mode t)
   :hook
   (after-init . global-company-mode) ;; Enable auto completion globally
   )
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Syntax checking
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package flycheck
   :ensure t
   :hook
   (after-init . global-flycheck-mode))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package org
   :ensure t
   :init
   (setq org-log-done t))
 
+;; Lisp-ish settings
 (use-package rainbow-delimiters
   :ensure t)
 
@@ -190,16 +152,31 @@
 (use-package paredit
   :ensure t)
 
+(use-package lisp-mode
+  :mode ("\\.el\\'" . lisp-mode)
+  :hook
+  ((lisp-mode . paredit-mode)
+   (lisp-mode . rainbow-delimiters-mode)
+   (lisp-mode . subword-mode)
+   (lisp-mode . eldoc-mode)
+   (lisp-mode . show-paren-mode)))
+
 (use-package clojure-mode
   :ensure t
-  :mode "\\.clj*\\"
+  :mode (("\\.clj\\'" . clojure-mode)
+         ("\\.edn\\'" . clojure-mode)
+         ("\\.cljs\\'" . clojure-mode)
+         ("\\.cljc\\'" . clojure-mode))
   :hook
   ((clojure-mode . paredit-mode)
    (clojure-mode . rainbow-delimiters-mode)
    (clojure-mode . subword-mode)
+   (clojure-mode . eldoc-mode)
    (clojurescript-mode . paredit-mode)
    (clojurescript-mode . rainbow-delimiters-mode)
-   (clojurescript-mode . subword-mode))))
+   (clojurescript-mode . subword-mode)
+   (clojurescript-mode . eldoc-mode)
+   (clojurescript-mode . show-paren-mode)))
 
 (use-package cider
   :ensure t
@@ -209,24 +186,28 @@
   ((cider-mode . eldoc-mode)
    (cider-repl-mode . eldoc-mode)
    (cider-repl-mode . paredit-mode)
-   (cider-repl-mode . rainbow-delimiters-mode)))
+   (cider-repl-mode . rainbow-delimiters-mode)
+   (cider-repl-mode . show-paren-mode)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Language settings
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (load "my-ruby.el")
-;; (load "my-clojure.el")
-;; (load "my-sql.el")
-;; (load "my-golang.el")
-;; (load "my-javascript.el")
-;; ;; Racket settings
-;; (setq racket-racket-program "/Applications/Racket v6.3/bin/racket")
-;; (setq racket-raco-program "/Applications/Racket v6.3/bin/raco")
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; debug settings
-;;;(setq max-specpdl-size 5)
-;;;(setq debug-on-error t)
+(use-package multi-term
+  :ensure t
+  :config
+  (setq multi-term-program "/bin/zsh"))
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-completion-style (quote emacs))
+ '(package-selected-packages
+   (quote
+    (multi-term spacemacs-light use-package spacemacs-theme rainbow-mode rainbow-delimiters paredit magit helm-projectile flycheck exec-path-from-shell cider auto-package-update ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
